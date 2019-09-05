2b89f0897bb08b7fcbad5733d89c0cdb5e5faa06
WildOrc <gamerbear@bk.ru>
2019-09-05 16:33:14 +0300

    add card & start work DB

--- index.js
+++ index.js
@@ -1,36 +1,52 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const homeRoutes = require('./routes/home');
 const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
 const aboutRoutes = require('./routes/about');
+const cardRoutes = require('./routes/card');
 
 const app = express();
 const hbs = exphbs.create({
   defaultLayout: 'main',
   extname: 'hbs'
 });
+const db_p ='k6hMU23Aaq68QdYT';
+const db_url = `mongodb+srv://root:${db_p}@cluster0-s10rv.mongodb.net/test?retryWrites=true&w=majority`;
 
 app.engine(`hbs`,hbs.engine);
 app.set(`view engine`, `hbs`);
 app.set('views','views');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({extended : true}));
 
 app.use('/',homeRoutes);
 app.use('/add',addRoutes);
 app.use('/courses',coursesRoutes);
 app.use('/about',aboutRoutes);
-
-
-
-
+app.use('/card',cardRoutes);
 
 const PORT = process.env.PORT || 5555;
 
-app.listen(PORT, ()=> {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function start() {
+  try {
+    await mongoose.connect(db_url, {
+      useNewUrlParser: true
+    });
+    app.listen(PORT, ()=> {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
+
+
+
+
--- routes/courses.js
+++ routes/courses.js
@@ -1,11 +1,42 @@
 const {Router} = require('express');
 const router = Router();
+const Course = require('../models/course');
 
-router.get('/',(req, res) => {
+router.get('/',async (req, res) => {
+  const courses = await Course.getAll();
+
   res.render('courses', {
     title: 'Список курсов',
-    isCourses: true
+    isCourses: true,
+    courses
   });
 });
+
+router.get('/:id', async (req, res) => {
+  const course = await Course.getByID(req.params.id);
+  res.render('course', {
+    layout: 'empty',
+    title: `Курс ${course.title}`,
+    course
+  });
+
+});
+
+router.get('/:id/edit', async (req, res) => {
+  if (!req.query.allow) {
+    return res.redirect('/');
+  }
+  const course = await Course.getByID(req.params.id);
+  res.render('course-edit', {
+    title: `Редактировать курс ${course.title}`,
+    course
+  });
+});
+
+router.post('/edit', async (req, res) => {
+  const course = await Course.update(req.body);
+  res.redirect('/courses');
+});
+
 
 module.exports = router;
